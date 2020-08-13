e54cec97374491918d96f3a1e11a14a6becc785a
adamsylla97 <209474@edu.p.lodz.pl>
2020-08-13 19:22:54 +0200

    added fragments for whole app

---
+++ app/src/main/java/com/bgpApp/ui/wikipedia/WikipediaFragment.kt
@@ -0,0 +1,22 @@
+package com.bgpApp.ui.wikipedia
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bgpApp.R
+
+class WikipediaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wikipedia, container, false)
+    }
+
+
+}
